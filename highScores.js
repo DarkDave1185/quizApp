1360e7201368a1f1eeead3629c41d59206198505
DarkDave <darkdave1185@gmail.com>
2020-06-09 10:43:31 -0700

    added highscore pg with buttons.unable to get score to show correctly

--- highScores.js
+++ highScores.js
@@ -1,7 +1,12 @@
 const username=document.getElementById("username");
 const saveScore=document.getElementById("saveScore");
 const finalScore=document.getElementById("finalScore");
-const recentScore=localStorage.getItem("recentScore")
+const recentScore=localStorage.getItem("recentScore");
+
+const highScore=JSON.parse(localStorage.getItem("highScore")) || [];
+
+const maxHighScore=3
+console.log(highScore);
 finalScore.innerText=recentScore;
 
 username.addEventListener("keyup",()=>{
@@ -9,10 +14,19 @@ username.addEventListener("keyup",()=>{
     saveScore.disabled=!username.value;
 });
 
-
-
-
 saveHighScore = e => {
     console.log("clicked the save button");
     e.preventDefault();
+
+    const score={
+        score: Math.floor(Math.random()*100),
+        name: username.value
+    };
+
+    highScore.push(score);
+    highScore.sort((a, b)=>b.score-a.score);
+    highScore.splice(3);
+
+    localStorage.setItem("highScore", JSON.stringify(highScore));
+    Location.assign("/");
 };
