cf2270ca4f4aaef62a27ee0f08661c69cca4f838
DarkDave <darkdave1185@gmail.com>
2020-06-09 21:36:13 -0700

    added timer unable to get scores to show. unable to get timer to show but runs when checked on page code.

--- quiz.js
+++ quiz.js
@@ -161,3 +161,19 @@ increaseScore= num =>{
 };
 /*able to get random questions if page refreshed*/
 startApp();
+
+const minutes=5;
+let time=minutes*60;
+
+const countDownEl=document.getElementById("");
+
+setInterval(updateCountDown, 1000);
+function updateCountDown(){
+    const minutes=Math.floor(time/60);
+    let seconds=time%60;
+
+    seconds=seconds<10?"0"+ seconds:seconds;
+
+    countDownEl.innerHTML= "${minutes}:${seconds}";
+    time--;
+}
